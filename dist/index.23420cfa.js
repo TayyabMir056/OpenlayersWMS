e274634d9309a24edaeca8ed1487321e18f8fdec
Tayyab Mir <tayyabmir056@gmail.com>
2021-02-23 15:54:54 +0400

    legend added

--- dist/index.23420cfa.js
+++ dist/index.23420cfa.js
@@ -457,8 +457,7 @@ require('ol/ol.css');
 var _olMap = require('ol/Map');
 var _parcelHelpers = require("@parcel/transformer-js/lib/esmodule-helpers.js");
 var _olMapDefault = _parcelHelpers.interopDefault(_olMap);
-var _olSourceOSM = require('ol/source/OSM');
-var _olSourceOSMDefault = _parcelHelpers.interopDefault(_olSourceOSM);
+require('ol/source/OSM');
 var _olLayerTile = require('ol/layer/Tile');
 var _olLayerTileDefault = _parcelHelpers.interopDefault(_olLayerTile);
 var _olSourceTileWMS = require('ol/source/TileWMS');
@@ -467,8 +466,11 @@ var _olView = require('ol/View');
 var _olViewDefault = _parcelHelpers.interopDefault(_olView);
 var _olOverlay = require('ol/Overlay');
 var _olOverlayDefault = _parcelHelpers.interopDefault(_olOverlay);
+var _olSourceXYZ = require('ol/source/XYZ');
+var _olSourceXYZDefault = _parcelHelpers.interopDefault(_olSourceXYZ);
+require('ol/source/BingMaps');
 var wmsSource = new _olSourceTileWMSDefault.default({
-  url: 'http://mapdata.peie.om/geoserver/gwc/service/wms',
+  url: 'https://mapdata.peie.om/geoserver/gwc/service/wms',
   params: {
     'LAYERS': 'PEIE:vacancy12370',
     'TILED': true,
@@ -480,7 +482,10 @@ var wmsSource = new _olSourceTileWMSDefault.default({
   transition: 0
 });
 var layers = [new _olLayerTileDefault.default({
-  source: new _olSourceOSMDefault.default()
+  source: new _olSourceXYZDefault.default({
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    maxZoom: 19
+  })
 }), new _olLayerTileDefault.default({
   source: wmsSource
 })];
@@ -530,7 +535,7 @@ map.on('singleclick', function (evt) {
   }
 });
 
-},{"ol/ol.css":"6KKsq","ol/Map":"3a3gj","ol/source/OSM":"3hR8N","ol/layer/Tile":"f0iTT","ol/source/TileWMS":"3IFEZ","ol/View":"14fkO","ol/Overlay":"2UGJw","@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y"}],"6KKsq":[function() {},{}],"3a3gj":[function(require,module,exports) {
+},{"ol/ol.css":"6KKsq","ol/Map":"3a3gj","ol/source/OSM":"3hR8N","ol/layer/Tile":"f0iTT","ol/source/TileWMS":"3IFEZ","ol/View":"14fkO","ol/Overlay":"2UGJw","ol/source/BingMaps":"2VBRL","@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y","ol/source/XYZ":"6NW9w"}],"6KKsq":[function() {},{}],"3a3gj":[function(require,module,exports) {
 var _parcelHelpers = require("@parcel/transformer-js/lib/esmodule-helpers.js");
 _parcelHelpers.defineInteropFlag(exports);
 var _rendererCompositeJs = require('./renderer/Composite.js');
@@ -41633,6 +41638,313 @@ function appendParams(uri, params) {
   return uri + qs;
 }
 
-},{"@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y"}]},["3V1iX","2X3X1"], "2X3X1", "parcelRequire1520")
+},{"@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y"}],"2VBRL":[function(require,module,exports) {
+var _parcelHelpers = require("@parcel/transformer-js/lib/esmodule-helpers.js");
+_parcelHelpers.defineInteropFlag(exports);
+_parcelHelpers.export(exports, "quadKey", function () {
+  return quadKey;
+});
+var _StateJs = require('./State.js');
+var _StateJsDefault = _parcelHelpers.interopDefault(_StateJs);
+var _TileImageJs = require('./TileImage.js');
+var _TileImageJsDefault = _parcelHelpers.interopDefault(_TileImageJs);
+var _extentJs = require('../extent.js');
+var _tileurlfunctionJs = require('../tileurlfunction.js');
+var _tilecoordJs = require('../tilecoord.js');
+var _tilegridJs = require('../tilegrid.js');
+var _projJs = require('../proj.js');
+var _netJs = require('../net.js');
+/**
+* @module ol/source/BingMaps
+*/
+var __extends = undefined && undefined.__extends || (function () {
+  var extendStatics = function (d, b) {
+    extendStatics = Object.setPrototypeOf || ({
+      __proto__: []
+    }) instanceof Array && (function (d, b) {
+      d.__proto__ = b;
+    }) || (function (d, b) {
+      for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+    });
+    return extendStatics(d, b);
+  };
+  return function (d, b) {
+    extendStatics(d, b);
+    function __() {
+      this.constructor = d;
+    }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+  };
+})();
+function quadKey(tileCoord) {
+  var z = tileCoord[0];
+  var digits = new Array(z);
+  var mask = 1 << z - 1;
+  var i, charCode;
+  for (i = 0; i < z; ++i) {
+    // 48 is charCode for 0 - '0'.charCodeAt(0)
+    charCode = 48;
+    if (tileCoord[1] & mask) {
+      charCode += 1;
+    }
+    if (tileCoord[2] & mask) {
+      charCode += 2;
+    }
+    digits[i] = String.fromCharCode(charCode);
+    mask >>= 1;
+  }
+  return digits.join('');
+}
+/**
+* The attribution containing a link to the Microsoft® Bing™ Maps Platform APIs’
+* Terms Of Use.
+* @const
+* @type {string}
+*/
+var TOS_ATTRIBUTION = '<a class="ol-attribution-bing-tos" ' + 'href="https://www.microsoft.com/maps/product/terms.html" target="_blank">' + 'Terms of Use</a>';
+/**
+* @typedef {Object} Options
+* @property {number} [cacheSize] Initial tile cache size. Will auto-grow to hold at least the number of tiles in the viewport.
+* @property {boolean} [hidpi=false] If `true` hidpi tiles will be requested.
+* @property {string} [culture='en-us'] Culture code.
+* @property {string} key Bing Maps API key. Get yours at http://www.bingmapsportal.com/.
+* @property {string} imagerySet Type of imagery.
+* @property {boolean} [imageSmoothing=true] Enable image smoothing.
+* @property {number} [maxZoom=21] Max zoom. Default is what's advertized by the BingMaps service.
+* @property {number} [reprojectionErrorThreshold=0.5] Maximum allowed reprojection error (in pixels).
+* Higher values can increase reprojection performance, but decrease precision.
+* @property {import("../Tile.js").LoadFunction} [tileLoadFunction] Optional function to load a tile given a URL. The default is
+* ```js
+* function(imageTile, src) {
+*   imageTile.getImage().src = src;
+* };
+* ```
+* @property {boolean} [wrapX=true] Whether to wrap the world horizontally.
+* @property {number} [transition] Duration of the opacity transition for rendering.
+* To disable the opacity transition, pass `transition: 0`.
+*/
+/**
+* @typedef {Object} BingMapsImageryMetadataResponse
+* @property {number} statusCode The response status code
+* @property {string} statusDescription The response status description
+* @property {string} authenticationResultCode The authentication result code
+* @property {Array<ResourceSet>} resourceSets The array of resource sets
+*/
+/**
+* @typedef {Object} ResourceSet
+* @property {Array<Resource>} resources
+*/
+/**
+* @typedef {Object} Resource
+* @property {number} imageHeight The image height
+* @property {number} imageWidth The image width
+* @property {number} zoomMin The minimum zoom level
+* @property {number} zoomMax The maximum zoom level
+* @property {string} imageUrl The image URL
+* @property {Array<string>} imageUrlSubdomains The image URL subdomains for rotation
+* @property {Array<ImageryProvider>} [imageryProviders] The array of ImageryProviders
+*/
+/**
+* @typedef {Object} ImageryProvider
+* @property {Array<CoverageArea>} coverageAreas The coverage areas
+* @property {string} [attribution] The attribution
+*/
+/**
+* @typedef {Object} CoverageArea
+* @property {number} zoomMin The minimum zoom
+* @property {number} zoomMax The maximum zoom
+* @property {Array<number>} bbox The coverage bounding box
+*/
+/**
+* @classdesc
+* Layer source for Bing Maps tile data.
+* @api
+*/
+var BingMaps = /** @class*/
+(function (_super) {
+  __extends(BingMaps, _super);
+  /**
+  * @param {Options} options Bing Maps options.
+  */
+  function BingMaps(options) {
+    var _this = this;
+    var hidpi = options.hidpi !== undefined ? options.hidpi : false;
+    _this = _super.call(this, {
+      cacheSize: options.cacheSize,
+      crossOrigin: 'anonymous',
+      imageSmoothing: options.imageSmoothing,
+      opaque: true,
+      projection: _projJs.get('EPSG:3857'),
+      reprojectionErrorThreshold: options.reprojectionErrorThreshold,
+      state: _StateJsDefault.default.LOADING,
+      tileLoadFunction: options.tileLoadFunction,
+      tilePixelRatio: hidpi ? 2 : 1,
+      wrapX: options.wrapX !== undefined ? options.wrapX : true,
+      transition: options.transition
+    }) || this;
+    /**
+    * @private
+    * @type {boolean}
+    */
+    _this.hidpi_ = hidpi;
+    /**
+    * @private
+    * @type {string}
+    */
+    _this.culture_ = options.culture !== undefined ? options.culture : 'en-us';
+    /**
+    * @private
+    * @type {number}
+    */
+    _this.maxZoom_ = options.maxZoom !== undefined ? options.maxZoom : -1;
+    /**
+    * @private
+    * @type {string}
+    */
+    _this.apiKey_ = options.key;
+    /**
+    * @private
+    * @type {string}
+    */
+    _this.imagerySet_ = options.imagerySet;
+    var url = 'https://dev.virtualearth.net/REST/v1/Imagery/Metadata/' + _this.imagerySet_ + '?uriScheme=https&include=ImageryProviders&key=' + _this.apiKey_ + '&c=' + _this.culture_;
+    _netJs.jsonp(url, _this.handleImageryMetadataResponse.bind(_this), undefined, 'jsonp');
+    return _this;
+  }
+  /**
+  * Get the api key used for this source.
+  *
+  * @return {string} The api key.
+  * @api
+  */
+  BingMaps.prototype.getApiKey = function () {
+    return this.apiKey_;
+  };
+  /**
+  * Get the imagery set associated with this source.
+  *
+  * @return {string} The imagery set.
+  * @api
+  */
+  BingMaps.prototype.getImagerySet = function () {
+    return this.imagerySet_;
+  };
+  /**
+  * @param {BingMapsImageryMetadataResponse} response Response.
+  */
+  BingMaps.prototype.handleImageryMetadataResponse = function (response) {
+    if (response.statusCode != 200 || response.statusDescription != 'OK' || response.authenticationResultCode != 'ValidCredentials' || response.resourceSets.length != 1 || response.resourceSets[0].resources.length != 1) {
+      this.setState(_StateJsDefault.default.ERROR);
+      return;
+    }
+    var resource = response.resourceSets[0].resources[0];
+    var maxZoom = this.maxZoom_ == -1 ? resource.zoomMax : this.maxZoom_;
+    var sourceProjection = this.getProjection();
+    var extent = _tilegridJs.extentFromProjection(sourceProjection);
+    var scale = this.hidpi_ ? 2 : 1;
+    var tileSize = resource.imageWidth == resource.imageHeight ? resource.imageWidth / scale : [resource.imageWidth / scale, resource.imageHeight / scale];
+    var tileGrid = _tilegridJs.createXYZ({
+      extent: extent,
+      minZoom: resource.zoomMin,
+      maxZoom: maxZoom,
+      tileSize: tileSize
+    });
+    this.tileGrid = tileGrid;
+    var culture = this.culture_;
+    var hidpi = this.hidpi_;
+    this.tileUrlFunction = _tileurlfunctionJs.createFromTileUrlFunctions(resource.imageUrlSubdomains.map(function (subdomain) {
+      /** @type {import('../tilecoord.js').TileCoord}*/
+      var quadKeyTileCoord = [0, 0, 0];
+      var imageUrl = resource.imageUrl.replace('{subdomain}', subdomain).replace('{culture}', culture);
+      return (
+        /**
+        * @param {import("../tilecoord.js").TileCoord} tileCoord Tile coordinate.
+        * @param {number} pixelRatio Pixel ratio.
+        * @param {import("../proj/Projection.js").default} projection Projection.
+        * @return {string|undefined} Tile URL.
+        */
+        function (tileCoord, pixelRatio, projection) {
+          if (!tileCoord) {
+            return undefined;
+          } else {
+            _tilecoordJs.createOrUpdate(tileCoord[0], tileCoord[1], tileCoord[2], quadKeyTileCoord);
+            var url = imageUrl;
+            if (hidpi) {
+              url += '&dpi=d1&device=mobile';
+            }
+            return url.replace('{quadkey}', quadKey(quadKeyTileCoord));
+          }
+        }
+      );
+    }));
+    if (resource.imageryProviders) {
+      var transform_1 = _projJs.getTransformFromProjections(_projJs.get('EPSG:4326'), this.getProjection());
+      this.setAttributions((function (frameState) {
+        var attributions = [];
+        var viewState = frameState.viewState;
+        var tileGrid = this.getTileGrid();
+        var z = tileGrid.getZForResolution(viewState.resolution, this.zDirection);
+        var tileCoord = tileGrid.getTileCoordForCoordAndZ(viewState.center, z);
+        var zoom = tileCoord[0];
+        resource.imageryProviders.map(function (imageryProvider) {
+          var intersecting = false;
+          var coverageAreas = imageryProvider.coverageAreas;
+          for (var i = 0, ii = coverageAreas.length; i < ii; ++i) {
+            var coverageArea = coverageAreas[i];
+            if (zoom >= coverageArea.zoomMin && zoom <= coverageArea.zoomMax) {
+              var bbox = coverageArea.bbox;
+              var epsg4326Extent = [bbox[1], bbox[0], bbox[3], bbox[2]];
+              var extent_1 = _extentJs.applyTransform(epsg4326Extent, transform_1);
+              if (_extentJs.intersects(extent_1, frameState.extent)) {
+                intersecting = true;
+                break;
+              }
+            }
+          }
+          if (intersecting) {
+            attributions.push(imageryProvider.attribution);
+          }
+        });
+        attributions.push(TOS_ATTRIBUTION);
+        return attributions;
+      }).bind(this));
+    }
+    this.setState(_StateJsDefault.default.READY);
+  };
+  return BingMaps;
+})(_TileImageJsDefault.default);
+exports.default = BingMaps;
+
+},{"./State.js":"6G5no","./TileImage.js":"sKtx9","../extent.js":"5PMSh","../tileurlfunction.js":"2fA5K","../tilecoord.js":"2kkyx","../tilegrid.js":"1JyRQ","../proj.js":"4Wdsb","../net.js":"4XnCS","@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y"}],"4XnCS":[function(require,module,exports) {
+var _parcelHelpers = require("@parcel/transformer-js/lib/esmodule-helpers.js");
+_parcelHelpers.defineInteropFlag(exports);
+_parcelHelpers.export(exports, "jsonp", function () {
+  return jsonp;
+});
+var _utilJs = require('./util.js');
+function jsonp(url, callback, opt_errback, opt_callbackParam) {
+  var script = document.createElement('script');
+  var key = 'olc_' + _utilJs.getUid(callback);
+  function cleanup() {
+    delete window[key];
+    script.parentNode.removeChild(script);
+  }
+  script.async = true;
+  script.src = url + (url.indexOf('?') == -1 ? '?' : '&') + (opt_callbackParam || 'callback') + '=' + key;
+  var timer = setTimeout(function () {
+    cleanup();
+    if (opt_errback) {
+      opt_errback();
+    }
+  }, 10000);
+  window[key] = function (data) {
+    clearTimeout(timer);
+    cleanup();
+    callback(data);
+  };
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+},{"./util.js":"4k52K","@parcel/transformer-js/lib/esmodule-helpers.js":"5gA8y"}]},["3V1iX","2X3X1"], "2X3X1", "parcelRequire1520")
 
 //# sourceMappingURL=index.23420cfa.js.map
